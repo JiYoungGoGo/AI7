14bcff5445467745bdea3b127cea34bea22e9a7b
Jiyoung <chjy100418@naver.com>
2022-10-19 21:16:19 +0900

    첫 커밋 in bash

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 view =  [100,150,30]
-st.write("# Youtube view")
+st.write("# Youtube view???")
 st.write("## raw")
 view
 st.write("## bar chart")
---
+++ hello.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+print("Hello222")
+print("TEST")
+
+print("ggggg")
